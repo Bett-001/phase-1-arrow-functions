9bbd0853d59df59c62b439a9e9f9e0c9b5450071
Tonnie-debug-svg <tonniekbett@gmail.com>
2025-09-25 20:05:28 +0300

    Fixed arrow functions to pass codegrade

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1️Function expression - divide two numbers
+const divide = function(num1, num2) {
+  return num1 / num2;
+};
+
+// 2️Arrow function - square a number
+const square = x => x * x;
+
+// 3️Arrow function - add two numbers
+const add = (a, b) => a + b;
